0f6acfc38fcd908ad7eeea080c81cb80df2e82f4
ognjenjevremovic <jevremovic.ognjen@gmail.com>
2017-02-19 00:53:45 +0100

    Setting up the development environment

--- systemjs.config.js
+++ systemjs.config.js
@@ -1,23 +1,21 @@
 (function(global) {
-    'use strict';
-
     System.config({
-        path: {
-            npm: 'node_modules/'
+        paths: {
+            'npm:': 'node_modules/'
         },
         map: {
             app : 'app',
 
-            '@angular/core': 'npm:@angular/core',
-            '@angular/common': 'npm:@angular/common',
-            '@angular/compiler': 'npm:@angular/compiler',
-            '@angular/forms': 'npm:@angular/forms',
-            '@angular/http': 'npm:@angular/http',
-            '@angular/platform-browser': 'npm:@angular/platform-browser',
-            '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic',
-            '@angular/router': 'npm:@angular/router',
+            '@angular/core': 'npm:@angular/core/bundles/core.umd.min.js',
+            '@angular/common': 'npm:@angular/common/bundles/common.umd.min.js',
+            '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.min.js',
+            '@angular/forms': 'npm:@angular/forms/bundle/forms.umd.min.js',
+            '@angular/http': 'npm:@angular/http/bundles/http.umd.min.js',
+            '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.min.js',
+            '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+            '@angular/router': 'npm:@angular/router/bundles/router.umd.min.js',
 
-            rxjs: 'npm:rxjs'
+            'rxjs': 'npm:rxjs'
         },
         packages: {
             app: {
